2f244b41c78a06c826055039e6b16511879f9363
Goldgom <qym2005723@hotmail.com>
2026-08-31 23:28:21 +0800

    chore: align Android version with community release

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.12.0"
+        versionName = "1.0.0-community"
 
         buildConfigField("String", "SERVER_URL", "\"${configuredServerUrl.replace("\\", "\\\\").replace("\"", "\\\"")}\"")
 
