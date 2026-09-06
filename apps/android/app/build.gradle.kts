270bc771bde8846524acd4332f6bc46cb209d4ca
Goldgom <qym2005723@hotmail.com>
2026-09-06 17:56:08 +0800

    升级版本号

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.1.0-community.0"
+        versionName = "1.1.0-community.1"
 
         buildConfigField("String", "SERVER_URL", "\"${configuredServerUrl.replace("\\", "\\\\").replace("\"", "\\\"")}\"")
 
